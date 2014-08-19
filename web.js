8cb5889d8fec37022da244e897bf200131506c36
Lawrence Wu <lawrence@lawrencewu.me>
2014-08-19 05:08:30 -0400

    initial commit

---
+++ web.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var logfmt = require("logfmt");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(logfmt.requestLogger());
+
+// Serve index.html
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// Start the server
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
